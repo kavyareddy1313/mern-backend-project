c6ca78f070502703837f0a9ca338579e92e8a510
Kavyasr <kavyasri28@gmail.com>
2026-02-23 13:10:24 +0530

    added url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:5001/api"
+    baseURL: "https://mern-backend-project-qi2g.onrender.com"
 });
 
 API.interceptors.request.use(req => {
